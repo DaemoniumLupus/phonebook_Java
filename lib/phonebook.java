39cc42eaa025cde431430db8a60b3cbc9cfdc4c3
Ilya-Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-10-11 19:09:24 +0300

    Update file

--- lib/phonebook.java
+++ lib/phonebook.java
@@ -1,5 +1,62 @@
 package phonebook.lib;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.text.html.parser.Element;
+
 public class phonebook {
-    
+    private HashMap<String,ArrayList<String>> book;
+    private int maxCountPhoneNumbers = 0;
+    public phonebook(){
+        this.book = new HashMap<>(10); 
+    }
+
+    private void Entry(String nameStr,String valueStr){
+        ArrayList<String> buf = new ArrayList<String>();
+
+        if (this.book.containsKey(nameStr)){
+            if (null != this.book.get(nameStr)){
+                buf = this.book.get(nameStr);
+            }
+        }
+        if (null != valueStr){
+            buf.add(valueStr);
+        }
+        if (buf.size() > maxCountPhoneNumbers) maxCountPhoneNumbers = buf.size();
+
+        this.book.put(nameStr,buf);
+    }
+    private StringBuilder SearchElement(String str){
+        StringBuilder answBuilder = new StringBuilder();
+        if (this.book.containsKey(str)){
+            answBuilder.append(str);
+            answBuilder.append(" --> ");
+            answBuilder.append(this.book.get(str));         
+        }else{
+            answBuilder.append("Nothing found!");
+        }
+
+        return answBuilder;
+    }
+
+    public void Add(String nameStr){
+        if(this.book.containsKey(nameStr)){
+            System.out.println("This name alrady exist!");
+        }else{
+            Entry(nameStr, null);
+        }
+    } 
+    public void Add(String nameStr,String valueStr) { Entry(nameStr, valueStr);}
+    public void Add(String nameStr,String[] valueStr){
+        for(String value :valueStr){
+            Entry(nameStr, value);
+        }
+    }
+    public void Search(String nameStr){
+
+    }
+      
+            
 }
