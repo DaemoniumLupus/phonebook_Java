973d43f7b3b67f0049db8e00d2d352ad8b6bd270
Ilya-Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-10-12 21:56:37 +0300

    new

--- src/App.java
+++ src/App.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class App {
+    static String commandList = """ 
+        Add name - add
+        Add one number for name - add1
+        Add some numbers for name - add0
+        Search name - seaname
+        Search number - seanum""";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Phonebook book = new Phonebook();
+
+        String command = new String();
+
+        Scanner in = new Scanner(System.in);
+
+        String strName = new String();
+        String strNum = new String();
+
+        do{
+            System.out.println(commandList);
+            System.out.print("Enter command: ");
+            command = in.nextLine();
+            
+
+            switch (command) {
+                case "add":
+                    System.out.print("Enter name:");
+                    strName = in.nextLine();
+                    book.Add(strName);
+                    break;
+                case "add1":
+                    System.out.print("Enter name:");
+                    strName = in.nextLine();
+                    
+                    System.out.print("Enter number:");
+                    strNum = in.nextLine();
+                    
+                    book.Add(strName,strNum);
+                    break;
+                case "add0":
+                    System.out.print("Enter name:");
+                    strName = in.nextLine();
+                    
+                    System.out.print("Enter number:");
+                    strNum = in.nextLine();
+                    
+                    book.Add(strName,strNum);
+                    break;
+
+            
+                default:
+                    break;
+            }
+
+            System.out.println(book.All());
+        }while(!(command.equals("q")));
+        in.close();
     }
 }
---
+++ src/Phonebook.java
@@ -0,0 +1,90 @@
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+
+public class Phonebook {
+    private HashMap<String,ArrayList<String>> book;
+    private int maxCountPhoneNumbers = 0;
+
+    public Phonebook(){
+        this.book = new HashMap<>(); 
+    }
+
+    private void Entry(String nameStr,String valueStr){
+        ArrayList<String> buf = new ArrayList<String>();
+
+        if (this.book.containsKey(nameStr)){
+            if (null != this.book.get(nameStr)){
+                buf = this.book.get(nameStr);
+            }
+        }
+        if (null != valueStr){
+            buf.add(valueStr);
+        }
+        if (buf.size() > this.maxCountPhoneNumbers) this.maxCountPhoneNumbers = buf.size();
+
+        this.book.put(nameStr,buf);
+    }
+    private StringBuilder StrBuilder(String str){
+        StringBuilder answBuilder = new StringBuilder();
+        
+        if (str != null) {
+            answBuilder.append(str);
+            answBuilder.append(" --> ");
+            answBuilder.append(this.book.get(str));
+        }else {
+            answBuilder.append("Nothing found!");
+        }
+        return answBuilder;
+    }
+    private StringBuilder SearchElementForName(String searchingStr, boolean... pref){ 
+        if(pref[0]){ 
+            Set<String> setNames =this.book.keySet();
+            for(String name : setNames){
+                ArrayList<String> valueList = new ArrayList<>();
+                valueList.addAll(this.book.get(name));
+                for (String value : valueList){
+                    if  (value.equals(searchingStr)){
+                        return StrBuilder(name);
+                    }
+                }
+            }
+            return StrBuilder(null);
+        }else {
+            if (this.book.containsKey(searchingStr)){
+                return StrBuilder(searchingStr);         
+            }else return StrBuilder(null);   
+        }
+    }
+
+    public void Add(String nameStr){
+        if(this.book.containsKey(nameStr)){
+            System.out.println("This name alrady exist!");
+        }else{
+            Entry(nameStr, null);
+        }
+    } 
+    public void Add(String nameStr,String valueStr) { Entry(nameStr, valueStr);}
+    public void Add(String nameStr,String[] valueStr){
+        for(String value :valueStr){
+            Entry(nameStr, value);
+        }
+    }
+    public StringBuilder Search(String nameStr){
+
+        return SearchElementForName(nameStr);
+    }
+    public StringBuilder Search(String valueStr, boolean pref){
+        return SearchElementForName(valueStr,pref);
+    }
+      
+    public Set<Map.Entry<String,ArrayList<String>>> All(){
+        return book.entrySet();
+    }     
+}
